ad36b45f42febfebb8a549fd93abf9c30d4e6f50
Damian <damian.hallbauer@gmail.com>
2018-06-24 01:15:06 -0700

    add game folder contents again

---
+++ Escape/Game.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Escape
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        //------------------------------------------------------------------
+        public Game()
+        {
+            GraphicsDeviceManager graphics = new GraphicsDeviceManager (this);
+            
+            graphics.PreferredBackBufferWidth = 480;
+            graphics.PreferredBackBufferHeight = 800;
+
+            graphics.SupportedOrientations = DisplayOrientation.Portrait;
+
+            // Disable fixed framerate
+            graphics.SynchronizeWithVerticalRetrace = false;
+            IsFixedTimeStep = false;
+
+            Content.RootDirectory = "Content";
+
+            IsMouseVisible = true;
+
+            //   graphics.GraphicsProfile = GraphicsProfile.Reach;
+            graphics.GraphicsProfile = GraphicsProfile.HiDef; //for shader to compile, because it uses 16 bit floats
+
+            //Window.SetPosition (new Point (600, 125));
+        }
+
+        //------------------------------------------------------------------
+        protected override void Initialize()
+        {
+            Components.Add (new Traffic.Actions.Base.Manager (this));
+            Components.Add (new Tools.Timers.Manager (this));
+            Components.Add (new Traffic.Manager (this));
+            Components.Add (new Tools.Markers.Manager (this));
+            Components.Add (new Tools.Perfomance (this));
+
+            base.Initialize();
+        }
+
+        //------------------------------------------------------------------
+        protected override void Update (GameTime gameTime)
+        {
+            if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown (Keys.Escape))
+                Exit();
+         
+
+            ControlTimeScale();
+
+            base.Update (gameTime);
+        }
+
+        //------------------------------------------------------------------
+        private void ControlTimeScale()
+        {
+            float scale = 0.05f;
+
+            if (Keyboard.GetState ().IsKeyDown(Keys.D1))
+                Traffic.Settings.TimeScale -= scale;
+            if (Keyboard.GetState ().IsKeyDown (Keys.D2))
+                Traffic.Settings.TimeScale += scale;
+        }
+
+        //------------------------------------------------------------------
+        protected override void Draw (GameTime gameTime)
+        {
+            GraphicsDevice.Clear (Color.HotPink);
+
+            base.Draw (gameTime);
+        }
+    }
+}
---
+++ Escape/Readme.txt
@@ -0,0 +1,45 @@
+﻿
+
+
+Notes:
+
+
+temp issue, only builds and runs wht mg and mg windowsdx.
+
+issue building content.  must manuallly build the content with the MG tool GUI each fx build.
+
+TODO CHECK DROID SHADER BASICS IN MONOGAME investing in this be sure it all works for droind.
+sure it can be modified.
+
+android might not have the float render for pixels.   //TODO before investing much, vertify if shader like this can work with droid.
+//not sure if additive hdr blending is needed.
+
+in sharpdx, teh hrdBlendable is a 4 16bit floats, that 
+
+not sure if half vector is 32 bit float or 16
+
+in Vector4 its for floats
+
+see the cg fluid code for other gpu ways..
+
+
+
+comments by the 
+ return SharpDX.DXGI.Format.R16G16B16A16_Float;
+
+
+
+In Fluid/Unit.cs:
+
+> @@ -20,7 +20,7 @@ abstract public class Unit
+      
+case SurfaceFormat.HdrBlendable:
+    // TODO: This needs to check the graphics device and
+    // return the best hdr blendable format for the device.
+    return SharpDX.DXGI.Format.R16G16B16A16_Float;
+I realized just now the development was made with NVidia GeForce 750 Ti videocard
+But a problem occured with it so I pulled out the videocard from my PC and use Intel integrated graphics now
+Maybe it can be a reason for HdrBlendable crash
+
+I suggest to verify if your videocard supports required format Float 16 bits per channel
+As I know there are many restrictions with float format for GPU. Such restriction was a reason why I couldn't laucnh it on Android
